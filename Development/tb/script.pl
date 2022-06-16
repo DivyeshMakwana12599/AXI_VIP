5e42b875407ca0e8f2a7768694430660d4e68b9a
Sandip-Mali-136 <sandipmali2001@gmail.com>
2022-06-16 10:58:41 +0530

    sasd

--- Development/tb/script.pl
+++ Development/tb/script.pl
@@ -1,3 +1,3 @@
 #!/usr/bin/perl
 
-system("vcs -sverilog -full64 -debug_access+r ei_axi4_top.sv && ./simv -gui +READ_TEST +length=3 +size=2 +addr_type=0 +burst_type=1");
+system("vcs -sverilog -full64 -debug_access+r ei_axi4_top.sv && ./simv -gui +ei_axi4_SANITY_TEST +total_num_trans=2 +length=3 +size=2 +addr_type=0 +burst_type=1");
