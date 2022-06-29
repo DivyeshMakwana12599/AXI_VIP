c47236cea238655fae93670c0fc0321ad7524a36
Sandip-Mali-136 <sandipmali2001@gmail.com>
2022-06-29 12:02:25 +0530

    solve error

--- Development/test/ei_axi4_4kb_boundary_test.sv
+++ Development/test/ei_axi4_4kb_boundary_test.sv
@@ -33,7 +33,7 @@ class ei_axi4_4k_boundary_test_c extends ei_axi4_base_test_c;
   ei_axi4_write_transaction_c wr_trans;
   ei_axi4_test_config_c test_cfg;
   
-  function new(virtual ei_axi4_master_interface mst_vif, virtual ei_axi4_slave_interface slv_vif);
+  function new(virtual `MST_INTF mst_vif, virtual `SLV_INTF slv_vif);
     super.new(mst_vif, slv_vif);
     test_cfg = new();
   endfunction
